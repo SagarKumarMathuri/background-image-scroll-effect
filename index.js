f323f20573454471372c00484dbb5e424c93a13d
sagar kumar mathuri <sagarku2002@gmail.com>
2026-05-29 11:23:22 +0530

    some changes inside js file

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-const bgimageE1 = document.querySelector("bg-imahe")
+const bgimageEl = document.getElementById("bg-image")
 
 
-window.addEventlistencer("scroll", ()=>{
+window.addEventListener("scroll", () =>{
     updateImage();
 })
 
 function updateImage(){
-  bgimageE1.style.opacity = 1 - window.pageYOffset / 900;
-  bgimageE1.style.backgroundSize = 160 - window.pageYOffset / 12 + "%";
+  bgimageEl.style.opacity = 1 - window.pageYOffset / 900;
+  bgimageEl.style.backgroundSize = 160 - window.pageYOffset / 12 + "%";
 
 }
 
